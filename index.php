da031da1e53e1269209091443dfc5fb4bc91e2e6
yasir khairullah <yasirkhairullah383@gmail.com>
2024-11-24 10:11:33 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>hello, world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ea placeat earum iusto voluptas et tempore molestias! Libero, neque. Facere laborum modi suscipit exercitationem corporis aut. Corrupti, corporis! Aperiam, pariatur!</p>
 </body>
 </html>
